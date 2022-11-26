5d8c82dbbeb839a40776c40ae1da0c1592916a87
Natalia Rusu <bonapartik@gmail.com>
2022-11-26 22:11:14 +0000

    add game functionality

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 const prompt = require('prompt-sync')();
 const { Player } = require('./src/player.js');
-
-const original_rooms = require("./src/rooms.js").rooms;//{rooms:{}}
-
-
+const { createRooms } = require('./src/rooms.js');
 
 const startGame = () => {
   const player = new Player();
-  const rooms = structuredClone(original_rooms);//clone rooms
-  
-  console.log(`${rooms['1'].text}\n`, '');
-  const choice = prompt(`${rooms['1'].question}`, '\n');
-
-  console.log('you entered ' + Number(choice + 1));
- 
+  const rooms = createRooms();
+  let counter = 0;
+  for (const room of rooms) {
+    counter++;
+    const choise = room.promptOptions(); //numb 0||1
+    const lostHeart = room.processOption(choise); //number of lost hearts
+    player.looseHeart(lostHeart);
+    if (!player.isAlive()) {
+      console.log('---------- Sorry, you lost the game! ----------');
+      console.log(`You past throught ${counter} room(s)`);
+      break;
+    }
+  }
+  if(counter===rooms.length){
+    console.log('---------- Congratulations! You won! ----------');
+    console.log(`You have ${player.hearts} heart(s) left`);
+    console.log(`You past throught all ${counter} room(s)`);
+  }  
+  promptReset();
 };
 
-startGame()
-//
-//
+const promptReset = () => {
+  let userInput = prompt('Would you like to play again? [y / n] ');
+  if (userInput === 'y') {
+    startGame();
+  } else if (userInput === 'n') {
+    console.log('Bye bye!');
+  } else {
+    console.log('Please enter a valid answer');
+    userInput = prompt('Would you like to play again? [y / n]');
+  }
+};
+
+startGame();
+
